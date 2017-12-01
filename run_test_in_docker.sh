aef56bf61188b1eb065349d7dcae1fc61879ff68
Lars Lubkoll <lars.lubkoll@posteo.de>
2017-12-01 21:35:37 +0100

    [TRAVIS] Fix scripts

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
-whoami
+
 FENICS_SHARED="${FENICS_HOME}/shared"
 DEPS="${FENICS_HOME}/deps"
+INCLUDE_DIR=$HOME/include
+LIB_DIR=$HOME/lib
+TEST_DIR=$HOME/test
 
 # Install gtest
-sudo mkdir -p ${DEPS}
+mkdir ${DEPS}
 cd ${DEPS}
 git clone https://github.com/google/googletest.git
 cd googletest && mkdir -p build && cd build && cmake .. && make -j2
-sudo cp -r ../googletest/include/gtest /usr/local/include/
-sudo cp googlemock/gtest/lib*.a /usr/local/lib
+sudo cp -r ../googletest/include/gtest $INCLUDE_DIR
+sudo cp googlemock/gtest/lib*.a $LIB_DIR
 
 # Install FunG
 cd ${DEPS}
 git clone https://github.com/lubkoll/FunG.git
-sudo cp -r FunG/fung /usr/local/include
+sudo cp -r FunG/fung $INCLUDE_DIR
 
 # Install Spacy
 cd ${DEPS}
 git clone https://github.com/spacy-dev/Spacy.git
-cd Spacy && mkdir -p build && cd build && cmake .. -DDolfin=ON && make -j2 && sudo make install
+cd Spacy && mkdir -p build && cd build && cmake .. -DDolfin=ON -DCMAKE_INSTALL_PREFIX=$HOME && make -j2 && sudo make install
 
 # Run tests
-cd ${FENICS_SHARED}/FEniCS
+cp $FENICS_SHARED/* $TEST_DIR
+
+cd ${TEST_DIR}/FEniCS
 ffc -l dolfin LinearHeat.ufl
 ffc -l dolfin L2Functional.ufl
 
-cd ${FENICS_SHARED}
+cd ${TEST_DIR}
 mkdir -p build && cd build
 cmake .. -DDolfin=ON -DCMAKE_CXX_STANDARD=14
 make -j2 && ctest
