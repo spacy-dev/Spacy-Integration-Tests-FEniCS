75fbb82810c874f61deb95b1aa024aaf13779e3e
Lars Lubkoll <lars.lubkoll@posteo.de>
2021-03-11 12:43:03 +0100

    Use conan for gtest

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+set -e
+
+sudo apt update
+sudo apt install python3 python3-pip python3-venv liblapacke-dev -y
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+export PATH="${PATH}:/home/dealii/.local/bin"
+pipx install conan
+conan profile new default
+conan profile update settings.compiler=gcc default
+conan profile update settings.compiler.version=9 default
+conan profile update settings.arch=x86_64 default
+conan profile update settings.os=Linux default
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
+sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3 99
+
 FENICS_SHARED="${FENICS_HOME}/shared"
 DEPS="${FENICS_HOME}/deps"
 INCLUDE_DIR=$HOME/include
@@ -11,22 +28,19 @@ mkdir $INCLUDE_DIR
 mkdir $LIB_DIR
 export PATH=$PATH:$INCLUDE_DIR:$LIB_DIR
 
-# Install gtest
-cd ${DEPS}
-git clone https://github.com/google/googletest.git
-cd googletest && mkdir -p build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=$HOME
-cmake --build . && cmake --build . --target install
-
 # Install FunG
-cd ${DEPS}
-git clone https://github.com/lubkoll/FunG.git
-cp -r FunG/fung $INCLUDE_DIR
+cd $DEPS
+git clone https://github.com/lubkoll/FunG
+cd FunG && mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$INCLUDE_DIR && cmake --build . --target install
 
 # Install Spacy
 cd ${DEPS}
 git clone https://github.com/spacy-dev/Spacy.git
-cd Spacy && mkdir -p build && cd build && cmake .. -DDolfin=ON -DCMAKE_INSTALL_PREFIX=$HOME -DCMAKE_CXX_FLAGS=-I/usr/local/lib/python3.6/dist-packages/ffc/backends/ufc
-cmake --build . && cmake --build . --target install
+cd Spacy && mkdir -p build && cd build
+conan install .. --build=missing
+cmake .. -DDolfin=ON -DCMAKE_INSTALL_PREFIX=$HOME -DCMAKE_CXX_FLAGS=-I/usr/local/lib/python3.6/dist-packages/ffc/backends/ufc -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake
+cmake --build . --target install
 
 # Run tests
 mkdir $TEST_DIR
